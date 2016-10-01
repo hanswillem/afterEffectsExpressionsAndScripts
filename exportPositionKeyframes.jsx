735001eb36253fc987287e61b08439056d8540a2
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-10-01 14:32:59 +0200

    Update exportPositionKeyframes.jsx

--- exportPositionKeyframes.jsx
+++ exportPositionKeyframes.jsx
@@ -4,6 +4,7 @@ Copyright: Hans Willem Gijzel (hanswillemgijzel@gmail.com)
 After Effects 2015.3 release / version 13.8.0.144
 */
 
+
 // get the position keyframes of the selected layer
 function getKeyframes() {
     p = [];
@@ -12,7 +13,7 @@ function getKeyframes() {
     var maxFrames = comp.duration * 30;
 
     for (var frame = 0; frame < maxFrames; frame++) {
-        var pos = lyr.transform.position.valueAtTime(1 / 30 * frame, true); // Note that '30' is the fps!!
+        var pos = lyr.transform.position.valueAtTime(comp.frameDuration * frame, true); 
         posx = pos[0];
         posy = pos[1];
         p.push([posx, posy, '*']);
